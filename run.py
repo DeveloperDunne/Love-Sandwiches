a148e32bf47a2fefd222f9cd08caf807674d3d61
carl.dunne <developerdunne@gmail.com>
2023-12-05 16:17:56 +0000

    Added validation to ensure values are intergers

--- run.py
+++ run.py
@@ -33,7 +33,9 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted,
     or if there arnt exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values are required, you provided {len(values)}"
